aa6fe65b0b81707f3f6a7023fd37adee29b12b5d
周健伟 <32414836+zwlovefish@users.noreply.github.com>
2026-04-30 21:22:15 +0800

    添加frp脚本

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 添加frp
+git clone https://github.com/kuoruan/openwrt-frp.git package/frp
