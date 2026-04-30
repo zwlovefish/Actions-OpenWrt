033a8c1bf4705bffbbd9c06292aa851a8ec7ec95
周健伟 <32414836+zwlovefish@users.noreply.github.com>
2026-05-01 00:24:37 +0800

    将自定义的包放到diy-part1.sh中
    Comment out the addition of the helloworld feed source and add a command to clone the frp package.

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+# 添加frp
+git clone https://github.com/kuoruan/openwrt-frp.git package/frp
