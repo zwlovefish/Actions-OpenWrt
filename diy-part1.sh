764f88c9d530ef11bdb4164fc4c8fd9768c3f646
周健伟 <32414836+zwlovefish@users.noreply.github.com>
2026-05-02 21:35:09 +0800

    去除frp相关
    Removed cloning of the frp package from the script.

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,10 +17,6 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-
-# 添加frp
-git clone https://github.com/kuoruan/openwrt-frp.git package/frp
-
 # 添加openclash
 git clone https://github.com/vernesong/OpenClash.git
 mv OpenClash/luci-app-openclash/ package/
