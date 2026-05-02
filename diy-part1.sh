24f132492a29386e4b57981d25a4b7e4dd3f9b7d
周健伟 <32414836+zwlovefish@users.noreply.github.com>
2026-05-02 14:58:53 +0800

    删除openclash

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,18 +20,3 @@
 
 # 添加frp
 git clone https://github.com/kuoruan/openwrt-frp.git package/frp
-
-# Clone 项目
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
-
-# 编译 po2lmo (如果有po2lmo可跳过)
-pushd luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
