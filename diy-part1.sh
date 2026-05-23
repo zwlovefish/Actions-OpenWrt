fb95e1e2bdb6806f6d0b0e45aa22cf1732cb031a
周健伟 <32414836+zwlovefish@users.noreply.github.com>
2026-05-23 10:18:18 +0800

    只有frpc的版本

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,3 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 添加openclash
-git clone https://github.com/vernesong/OpenClash.git
-mv OpenClash/luci-app-openclash/ package/
